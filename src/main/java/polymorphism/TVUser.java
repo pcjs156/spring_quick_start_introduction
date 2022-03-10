59268dd55ad575ed9603f7bf0e08fb3c03941a12
Yubin Park <itqkrdbqls1001@naver.com>
2022-03-11 02:01:45 +0900

    feat: 2.3.3. 디자인 패턴 이용하기
    Factory 패턴 사용

--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -2,7 +2,11 @@ package polymorphism;
 
 public class TVUser {
     public static void main(String[] args) {
-        TV tv = new LGTV();
+        BeanFactory factory = new BeanFactory();
+
+        // 원래는 (TV) factory.getBean(args[0]); 인데, 그냥 삼성으로 고정함
+        // 클라이언트 소스를 따로 수정하지 않고 문자열(beanName param)만 바꿀 수 있다는 점이 중요
+        TV tv = (TV) factory.getBean("samsung");
         tv.turnOn();
         tv.volumeUp();
         tv.volumeDown();
