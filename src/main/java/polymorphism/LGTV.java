28b9045e4b928cf97d79b781f499b665078a45cf
Yubin Park <itqkrdbqls1001@naver.com>
2022-03-11 17:01:08 +0900

    feat: 5.1. 어노테이션 설정 기초
    @Component annotation을 사용해 Java source code에서 bean 지정을 수행했다.

--- src/main/java/polymorphism/LGTV.java
+++ src/main/java/polymorphism/LGTV.java
@@ -1,7 +1,16 @@
 package polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component("tv")
 public class LGTV implements TV {
     protected final String brandName = "LGTV";
+
+    public LGTV() {
+        System.out.println("===> " + brandName + " 객체 생성");
+    }
 
     public void turnOn() {
         System.out.println(brandName + "---" + "전원 켠다.");
@@ -17,5 +26,10 @@ public class LGTV implements TV {
 
     public void volumeDown() {
         System.out.println(brandName + "---" + "소리 내린다.");
+    }
+
+    public static void main(String[] args) {
+        AbstractApplicationContext ctx = new GenericXmlApplicationContext("applicationContext.xml");
+        TV tv = (LGTV) ctx.getBean("tv");
     }
 }
